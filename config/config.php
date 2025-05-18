5da5fae8ba97ea51770c2462946a42a264801abf
Daniel <danielkibret8@gmail.com>
2025-05-18 23:07:58 +0300

    backend

--- config/config.php
+++ config/config.php
@@ -8,13 +8,13 @@ if (session_status() === PHP_SESSION_NONE) {
 define("SITE_TITLE", "Greenfield School");
 define("ADMIN_EMAIL", "your-email@example.com"); 
 
-// Database credentials
+
 define("DB_HOST", "localhost");
 define("DB_USER", "root");        
 define("DB_PASS", "");             
 define("DB_NAME", "school_website");
 
-// Create database connection
+
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($conn->connect_error) {
